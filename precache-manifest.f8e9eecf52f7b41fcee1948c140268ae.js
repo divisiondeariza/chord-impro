036ce4e7ea4b401e9624b0008c2a6b1d8df5ad7a
emmanuel <ejariza@gmail.com>
2020-01-11 20:42:12 -0500

    Updates

---
+++ precache-manifest.f8e9eecf52f7b41fcee1948c140268ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0867e18c6c3bb605099db1028d228ce",
+    "url": "/chord-impro/index.html"
+  },
+  {
+    "revision": "302acab51fc2c68f9d49",
+    "url": "/chord-impro/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ddab087cdcb2cd6e5490",
+    "url": "/chord-impro/static/css/main.9305b842.chunk.css"
+  },
+  {
+    "revision": "302acab51fc2c68f9d49",
+    "url": "/chord-impro/static/js/2.2ad0c5bd.chunk.js"
+  },
+  {
+    "revision": "6927c6c2aa28378d514655499e49ee93",
+    "url": "/chord-impro/static/js/2.2ad0c5bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddab087cdcb2cd6e5490",
+    "url": "/chord-impro/static/js/main.3a1e38ce.chunk.js"
+  },
+  {
+    "revision": "55b1c74a0a569d3fb0d4",
+    "url": "/chord-impro/static/js/runtime-main.09a88697.js"
+  }
+]);
